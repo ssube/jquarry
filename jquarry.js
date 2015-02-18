e9c03a87c8e3e563ef4b22a8cbfe26b09011be03
ssube <ssube@redbrickhealth.com>
2015-02-18 10:48:40 -0600

    Added initial site and script v1.

---
+++ jquarry.js
@@ -0,0 +1,8 @@
+/**
+ * jQuarry v1.0.0, (c) 2015 by Sean Sube
+ *
+ * This library may be used or modified in any legal fashion, but attribution is totally required.
+ */
+
+jQuery.noConflict();
+var Ҩ = jQuery;
